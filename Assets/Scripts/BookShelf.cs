4c441b46d6c4dcba5879f014e7f49f6404f365f7
Andrés <demiurgosoft@hotmail.com>
2016-02-05 16:41:57 +0100

    Interaction user-shelf

---
+++ Assets/Scripts/BookShelf.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BookShelf : TextDownload {
+	
+	void Start () {
+	
+	}
+	
+	void Interaction(){
+		Debug.Log ("Interaction");
+		BookPosition book;
+		book.room = "1";
+		book.wall = 1;
+		book.shelf = 1;
+		book.volume = 1;
+		book.page = 1;
+		GetPage(book);
+	}
+
+	
+	protected override void OnPage (string page)
+	{
+		Debug.Log ("On Page:" + page);
+	}
+	
+	protected override void OnTitle (string title)
+	{
+		Debug.Log ("On Title:" + title);
+	}
+}
---
+++ Assets/Scripts/User.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class User : MonoBehaviour {
+	//private Camera camera
+	// Use this for initialization
+	void Start () {
+		//camera = GetComponentInChildren<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.E)) {
+			GameObject obj=GetFrontObject ();
+			if(obj!=null) sendAction (obj);
+		}
+	}
+
+	void sendAction(GameObject target){
+		target.SendMessageUpwards ("Interaction");
+	}
+	//Raycast in camera direction, returns gameobject
+	GameObject GetFrontObject(){
+		RaycastHit hit;
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+		if (Physics.Raycast (transform.position, fwd, out hit,100)) {
+			return hit.transform.gameObject;
+		} else
+			return null;
+	}
+}
